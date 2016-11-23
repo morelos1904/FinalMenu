58c3769248024e3d73daab5d7c6e148e22863c7a
Eder Morelos <emorelos@nearbpo.com>
2016-11-22 18:38:42 -0600

    Menu

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,39 @@ var gulp = 			require ('gulp'),
 	browserSync=	require	('browser-sync'),
 	reload=			browserSync.reload,
 	uglify= 		require ('gulp-uglify'),
-	rename=			require ('gulp-rename');
+	rename=			require ('gulp-rename'),
+	sass = 			require('gulp-sass'),
+	sourcemaps = 	require('gulp-sourcemaps');
 
 /////////////////////////////////
 ///Scripts tasks
 /////////////////////////////////
 gulp.task('scripts',function(){
-	gulp.src(['app/js/*.js'])			
+	gulp.src(['app/js/**/*.js'])			
 	.pipe(reload({stream:true}));
 });	
-
+/////////////////////////////////
+///Html tasks
+/////////////////////////////////
+gulp.task('html',function(){
+	gulp.src('app/*.html')
+	.pipe(reload({stream:true}));
+});
+/////////////////////////////////
+///Sass tasks
+/////////////////////////////////
+gulp.task('sass', function() {
+	gulp.src('app/scss/**/*.scss')
+	// Initializes sourcemaps
+	.pipe(sourcemaps.init())
+	.pipe(sass({
+		errLogToConsole: true
+	}))
+	// Writes sourcemaps into the CSS file
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('app/css/'))
+	.pipe(reload({stream:true}));
+});
 /////////////////////////////////
 ///Browser-Sync tasks
 /////////////////////////////////
@@ -23,21 +46,20 @@ gulp.task('browser-sync',function(){
 	}) 	
 });
 /////////////////////////////////
-///Html tasks
-/////////////////////////////////
-gulp.task('html',function(){
-	gulp.src('app/*.html')
-	.pipe(reload({stream:true}));
-});
-/////////////////////////////////
 ///Watch tasks
 /////////////////////////////////
 gulp.task('watch',function(){
-	gulp.watch('app/js/*.js',['scripts']); 	//Every change that we made,it will run our function script
+	gulp.watch('app/js/**/*.js',['scripts']); 	//Every change that we made,it will run our function script
 	gulp.watch('app/*.html',['html']); 	//Every change that we mad
+	gulp.watch('app/scss/**/*.scss',['sass']); 	//Every change that we mad
 });
-
 /////////////////////////////////
 ///Default
 /////////////////////////////////
-gulp.task('default',['scripts','html','browser-sync','watch']);
+gulp.task('default',['scripts','html','sass','browser-sync','watch']);
+
+
+
+
+
+
